51f7f6886be48501f0a916aff3ba5eb6e88df74f
bsw071923 <71660922+bsw071923@users.noreply.github.com>
2020-10-15 22:31:03 +0900

    Create week3_sweet.py

---
+++ week3_sweet.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# 아래 빈 칸('')을 채워보세요
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+print(data)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+# 아래 빈 칸('')을 채워보세요
+for tr in trs:
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+    rank = tr.select_one('td.number').text[0:2].strip()
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+    title = tr.select_one('a.title').text.strip()
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+    artist = tr.select_one('a.artist').text
+    print(rank, title, artist)
